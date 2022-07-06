000f861574d1ffac1e34223e1ee3230542fe0890
kadnaoui <abderrahimkdn@gmail.com>
2022-07-06 17:19:07 +0100

    adding hed

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Header from "./Header";
 import Input from "./input";
+import DocumentMeta from 'react-document-meta';
 
 
 export default function App() {
 
-
+    const meta = {
+        title: 'MemeApp',
+        description: 'Generate a Random meme and add your ideas to it',
+        meta: {
+          charset: 'utf-8',
+          name: {
+            keywords: 'react,portfolio,fun,app,build,Abderrahim Kadnaoui'
+          }
+        }
+      };
     return (
-        <> 
+   
+        <DocumentMeta {...meta}>
         <Header />
         <Input />
+      </DocumentMeta>
 
-        </>
 
     )
 }
